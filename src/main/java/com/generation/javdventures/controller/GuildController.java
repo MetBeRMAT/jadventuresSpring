9f256c80d9464039417fd2efd541fa43b7250afa
Lorenzo Antonucci <lorenzo.antonucci98@gmail.com>
2024-02-22 12:46:32 +0100

    ServiceTry

--- src/main/java/com/generation/javdventures/controller/GuildController.java
+++ src/main/java/com/generation/javdventures/controller/GuildController.java
@@ -14,7 +14,7 @@ import com.generation.jadventures.model.repositories.GuildRepository;
 
 @RestController
 @CrossOrigin
-
+ 
 public class GuildController {
     @Autowired
     GuildRepository repo;
