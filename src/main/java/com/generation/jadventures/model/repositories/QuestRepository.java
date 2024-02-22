84bd63aa6ddd907784068dd5ee3dd5193b7d2d5a
Alessandro Cervoni <a.cerv@outlook.it>
2024-02-22 11:55:55 +0100

    controller

--- src/main/java/com/generation/jadventures/model/repositories/QuestRepository.java
+++ src/main/java/com/generation/jadventures/model/repositories/QuestRepository.java
@@ -2,6 +2,8 @@ package com.generation.jadventures.model.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.generation.jadventures.entities.Quest;
+
 public interface QuestRepository extends JpaRepository<Quest,Integer>
 {
 
--- src/main/java/com/generation/javdventures/controller/GuildController.java
+++ src/main/java/com/generation/javdventures/controller/GuildController.java
@@ -1,10 +1,16 @@
 package com.generation.javdventures.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.jadventures.entities.Guild;
+import com.generation.jadventures.model.repositories.GuildRepository;
 
 @RestController
 @CrossOrigin
