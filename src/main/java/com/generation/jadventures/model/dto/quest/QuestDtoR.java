a8cea3e987646f83989e13b0db2572b822bb5b89
GAETANO <piegaetano51@gmail.com>
2024-02-22 12:22:48 +0100

    v1

--- src/main/java/com/generation/jadventures/model/dto/quest/QuestDtoR.java
+++ src/main/java/com/generation/jadventures/model/dto/quest/QuestDtoR.java
@@ -1,5 +1,11 @@
 package com.generation.jadventures.model.dto.quest;
 
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@Data
+
 public class QuestDtoR extends QuestDtoBase
 {
     public QuestDtoR(){}
