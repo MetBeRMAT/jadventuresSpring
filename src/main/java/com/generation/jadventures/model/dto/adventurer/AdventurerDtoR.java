7fd2aa6825208dbbbac9a3d707d0063cbd169b8a
GAETANO <piegaetano51@gmail.com>
2024-02-23 11:46:47 +0100

    v1

---
+++ src/main/java/com/generation/jadventures/model/dto/adventurer/AdventurerDtoR.java
@@ -0,0 +1,15 @@
+package com.generation.jadventures.model.dto.adventurer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@Data
+@AllArgsConstructor
+
+public class AdventurerDtoR 
+{
+    public AdventurerDtoR(){}
+    private Integer id;
+}
