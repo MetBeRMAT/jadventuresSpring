74c7f7efe992e16c1767dd1b9c1ccfac4b58dfc1
GAETANO <piegaetano51@gmail.com>
2024-02-23 15:33:42 +0100

    v1

--- src/main/java/com/generation/jadventures/model/dto/party/PartyDtoWQuest.java
+++ src/main/java/com/generation/jadventures/model/dto/party/PartyDtoWQuest.java
@@ -3,6 +3,11 @@ package com.generation.jadventures.model.dto.party;
 import java.util.List;
 import com.generation.jadventures.entities.Quest;
 
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+@SuperBuilder
+@Data
+
 public class PartyDtoWQuest extends PartyDtoBase
 {
     public PartyDtoWQuest(){}
